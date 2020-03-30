9c346a5c3d4e5becb10db2dd439e280cadfaadcc
kirk <vtechjm@gmail.com>
2020-03-29 23:08:17 -0500

    clean up

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,12 +6,8 @@ public class Driver {
 
     public static void main(String[] args)
     {
-        //Connecting to Redis server on localhost
-//        Jedis jedis = new Jedis("localhost");
        Jedis jedis = RedisProvider.getInstance().getJedisPoolInstance();
-
-
-        //check whether server is running or not
+       //check whether server is running or not
         if(jedis.ping().equals("PONG"))
         { System.out.println("Redis is running");
           jedis.set("data","purge");
@@ -20,15 +16,8 @@ public class Driver {
             jedis.sadd("Request1", "123456785");
             jedis.sadd("Request1", "444444444");
 
-
             Set<String> dataSet = jedis.smembers("Request1");
             dataSet.forEach(System.out::println);
-
-//        jedis.get("son");
-
         }
-
-
-
     }
 }
